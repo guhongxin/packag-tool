f382b0ef7f6c3767897ef91df3d545afee4f95cb
guhongxin <1204738681@qq.com>
2020-09-21 11:31:35 +0800

    123

--- src/renderer/views/home/index.vue
+++ src/renderer/views/home/index.vue
@@ -1,26 +1,87 @@
 <template>
-  <div>首页
-    <input type="file" webkitdirectory/>
-    <button @click="test">通知</button>
+  <div class="home">
+    <div class="game-config">
+      <el-form ref="form" :model="form" label-width="120px" size="small" class="form">
+        <el-form-item label="产品状态">
+          <el-input v-model="form.status"></el-input>
+        </el-form-item>
+        <el-form-item label="产品参数1" class="w-form-item">
+          <el-input v-model="form.status"></el-input>
+          <i class="input-ad-icon el-icon-circle-plus"></i>
+        </el-form-item>
+        <el-form-item label="通知地址">
+          <el-input v-model="form.status"></el-input>
+        </el-form-item>
+        <el-form-item label="母包地址" class="w-form-item">
+          <el-input v-model="form.status"></el-input>
+          <el-button type="primary" class="input-btn" @click="openFile('openFile')">选择母包</el-button>
+        </el-form-item>
+        <el-form-item label="输出路径" class="w-form-item">
+          <el-input v-model="form.status"></el-input>
+          <el-button type="primary" class="input-btn" @click="openFile('openDirectory')">存放路径</el-button>
+        </el-form-item>
+        <el-form-item label="签名证书" class="w-form-item">
+          <el-input v-model="form.status"></el-input>
+          <el-button type="primary" class="input-btn" @click="openFile('openFile')">签名证书</el-button>
+        </el-form-item>
+        <el-form-item label="输出文件名称">
+          <el-input v-model="form.status"></el-input>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      form: {}
+    }
+  },
   methods: {
-    test () {
-      console.log('this.$electron')
-      let kk = this.$electron.remote.dialog.showOpenDialog({
-        title: 'kk',
-        properties: ['openDirectory']
-      })
-      console.log('kk', kk)
-      // const { shell } = this.$electron.remote
-      // shell.showItemInFolder()
+    openFile (param) {
+      // 打开文件 是否是文件夹
+      let _properties = param || 'openFile'
+      let path = this.$electron.remote.dialog.showOpenDialog({ properties: [_properties] })
+  
+    },
+    openCatalog () {
+      // 打开目录
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-
+.home {
+  height: 0px;
+}
+.game-config {
+  width: 50%;
+  margin: 40px auto 0px;
+  box-sizing: border-box;
+  padding: 20px;
+  background-color: #ffffff;
+  .form {
+    width: 80%;
+    .w-form-item {
+      position: relative;
+      .input-btn {
+        position: absolute;
+        right: -90px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      .input-ad-icon {
+        font-size: 16px;
+        position: absolute;
+        right: -25px;
+        top: 50%;
+        transform: translateY(-50%);
+        color: #5cb6ff;
+        cursor: pointer;
+      }
+    }
+  }
+}
 </style>
--- src/renderer/views/layout/components/SideBar.vue
+++ src/renderer/views/layout/components/SideBar.vue
@@ -5,7 +5,22 @@
     </div>
     <div class="game">
       <div class="game-logo">
-        <img src="../../../assets/image/logo.png" />
+        <img src="../../../assets/image/rmgame.png" />
+      </div>
+      <div class="search-box">
+        <el-input
+          placeholder="请输入内容"
+          size="small"
+          v-model="input4">
+          <i slot="suffix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+      </div>
+      <div class="game-list">
+        <ul>
+          <li class="game-item active-game">末世强袭</li>
+          <li class="game-item">血色烂漫</li>
+          <li class="game-item">王者荣耀</li>
+        </ul>
       </div>
     </div>
     <div class="edition">版本号：v-101</div>
@@ -13,23 +28,74 @@
 </template>
 <script>
 export default {
-  name: 'sidebar'
+  name: 'sidebar',
+  data () {
+    return {
+      input4: ''
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
 .side-bar {
   position: relative;
   .logo {
-    width: 100%;
-    height: 50px;
-    box-sizing: border-box;
-    padding: 5px 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    img {
       width: 100%;
-      display: block;
+      height: 50px;
+      box-sizing: border-box;
+      padding: 5px 5px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+  .game {
+    margin-top: 40px;
+    padding: 0px 10px;
+    box-sizing: border-box;
+    .game-logo {
+      width: 40%;
+      margin: 0px auto;
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+    .search-box {
+      margin: 40px 0px;
+      /deep/ .el-input__inner{
+        border-radius: 16px;
+      }
+    }
+    .game-list {
+      margin-left: -10px;
+      margin-right: -10px;
+      ul {
+        margin: 0px;
+        padding: 0px;
+        li {
+          list-style-type:none;
+          color: #ffffff;
+          height: 40px;
+          line-height: 40px;
+          padding-left: 10px;
+          font-size: 14px;
+          font-weight: 600;
+          color: rgb(191, 203, 217);
+          cursor: pointer;
+        }
+        .active-game {
+          color: rgb(64, 158, 255);
+          background-color: rgb(38, 52, 69);
+        }
+        li:hover {
+          color: rgb(64, 158, 255);
+          background-color: rgb(38, 52, 69);
+        }
+      }
     }
   }
   .edition {
